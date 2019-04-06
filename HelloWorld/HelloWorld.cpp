d21bfc5e1f99cf4e6197f829b37acbb3ec6ebd8e
quyan <quyan.bupt@qq.com>
2019-04-06 04:44:05 -0400

    Hello World.

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 
 int main() {
+	//to compile this file which use c++11 : g++ HelloWorld.cpp -std=c++11 -o HelloWorld
 	std::cout << "Hello World!" << std::endl;
 	
 	std::vector<int> nums = {1, 2, 3, 4, 5};
@@ -12,12 +13,12 @@ int main() {
 	std::cout << std::endl;
 	
 	std::unordered_map<std::string, int> hashMap;
-	hashMap["quyan"] = 2018110086;
-	hashMap["liuxinyu"] = 2018110184;
+	hashMap["quyan"] = 2018110011;
+	hashMap["liuxinyu"] = 2018110111;
 
 	for (auto mPair : hashMap) {
 		std::cout << mPair.first << " : " << mPair.second << std::endl;
 	}
-	
+
 	return 0;
 }
