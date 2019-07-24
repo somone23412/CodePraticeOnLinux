f03800e437c44446ff0b262cbbdebc45dbc21821
quyan <quyan.bupt@qq.com>
2019-07-24 22:19:20 +0800

    add CMake/Demo5.

---
+++ CMake/Demo5/install.sh
@@ -0,0 +1,4 @@
+cmake .
+make
+mkdir include && mkdir bin
+make install
---
+++ CMake/Demo5/uninstall.sh
@@ -0,0 +1,3 @@
+rm Makefile cmake_install.cmake Demo CMakeCache.txt *.a *.o install_manifest.txt
+rm -rf CMakeFiles bin include
+
